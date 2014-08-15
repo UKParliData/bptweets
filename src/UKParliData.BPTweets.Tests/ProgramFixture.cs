8a27f2739b7c40adad121e0cce7f6ce27843daf0
James McKay <mckayj@parliament.uk>
2014-08-15 15:53:41 +0100

    Truncate long titles so we don't exceed the length of a tweet.

--- src/UKParliData.BPTweets.Tests/ProgramFixture.cs
+++ src/UKParliData.BPTweets.Tests/ProgramFixture.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Moq;
 using NUnit.Framework;
@@ -104,6 +105,38 @@ namespace UKParliData.BPTweets.Tests
             log.Verify(x => x.LogTweetedID(It.IsAny<string>()), Times.Exactly(5));
 
             Assert.AreEqual("SN02629", loggedIDs.Last());
+        }
+
+        [Test]
+        public void TruncatesTitleWhenTooLongForTweet()
+        {
+            var bp = new BriefingPaper() {
+                Date = new DateTime(2014, 1, 1, 12, 0, 0),
+                Description = "Lords Library Note",
+                Identifier = "LLN 2014/029",
+                Title = "The Impact of Personal Indebtedness in United Kingdom Households, Especially on Children",
+                Type = "Lords Library Note"
+            };
+
+            string tweetText = null;
+
+            var reader = new Mock<IBPReader>();
+            reader.Setup(x => x.ReadAll()).Returns(new BriefingPaper[] { bp });
+
+            var client = new Mock<ITwitterClient>();
+            client.Setup(x => x.Tweet(It.IsAny<string>())).Callback<string>(x => tweetText = x);
+
+            var log = new Mock<ITweetLog>();
+            log.Setup(x => x.GetTweetedIDs()).Returns(new HashSet<string>());
+
+            var app = new Program(reader.Object, log.Object, client.Object);
+            app.Run();
+
+            client.Verify(x => x.Tweet(It.IsAny<string>()), Times.Once);
+
+            tweetText = Regex.Replace(tweetText, "http://www.parliament.uk/.*$", String.Empty);
+
+            Assert.LessOrEqual(tweetText.Length + Program.ShortenedUrlLength, 140);
         }
     }
 }
--- src/UKParliData.BPTweets/Program.cs
+++ src/UKParliData.BPTweets/Program.cs
@@ -9,6 +9,9 @@ namespace UKParliData.BPTweets
 {
     public class Program
     {
+        public static readonly int ShortenedUrlLength
+            = Int32.Parse(ConfigurationManager.AppSettings["Twitter.ShortenedUrlLength"]);
+
         static void Main(string[] args)
         {
             var client = new TwitterClient();
@@ -46,12 +49,25 @@ namespace UKParliData.BPTweets
             var tweetedIDs = this.log.GetTweetedIDs();
             foreach (var bp in reader.ReadAll().Where(x => !tweetedIDs.Contains(x.Identifier)).OrderBy(x => x.Date))
             {
-                string tweet = String.Format("New {0}: {1} {2} http://www.parliament.uk/briefing-papers/{3}",
+                string tweet = String.Format("New {0}: {1} {2}",
                     bp.Type,
                     bp.Identifier,
-                    bp.Title,
+                    bp.Title
+                );
+
+                // 139 rather than 140 to allow for space between the title and the url
+                int maxLength = 139 - ShortenedUrlLength;
+
+                if (tweet.Length > maxLength)
+                {
+                    tweet = tweet.Substring(0, maxLength - 3) + "...";
+                }
+
+                tweet += String.Format(
+                    " http://www.parliament.uk/briefing-papers/{0}", 
                     bp.Identifier.Replace("/", "-").Replace(" ", "-")
                 );
+
                 client.Tweet(tweet);
                 log.LogTweetedID(bp.Identifier);
                 Console.WriteLine(tweet);
